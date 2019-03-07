4ffc6e1e3fd9b0a38b4c02a90e39d2f5b78de2d8
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-03-06 23:53:25 -0300

    Implementacao da classe JwtAuthenticationEntryPoint - trata os erros de autenticação

---
+++ src/main/java/com/cesarjunior/curso/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.cesarjunior.curso.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+//classe que tratará dos erros de autenticação.
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		
+	}
+
+}
